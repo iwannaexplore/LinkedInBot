f422c6e293561f9353f1276e1cb9a9de5e913998
Dima Senko <iwannaexplore@gmail.com>
2021-07-14 15:11:53 +0300

    Закончил парсер

--- Linkedin/Form1.Designer.cs
+++ Linkedin/Form1.Designer.cs
@@ -31,7 +31,7 @@ namespace Linkedin
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.MainLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -51,21 +51,21 @@ namespace Linkedin
             this.textBox1.Size = new System.Drawing.Size(145, 20);
             this.textBox1.TabIndex = 1;
             // 
-            // label1
+            // MainLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(77, 7);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Ссылка";
+            this.MainLabel.AutoSize = true;
+            this.MainLabel.Location = new System.Drawing.Point(77, 7);
+            this.MainLabel.Name = "MainLabel";
+            this.MainLabel.Size = new System.Drawing.Size(46, 13);
+            this.MainLabel.TabIndex = 2;
+            this.MainLabel.Text = "Ссылка";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(200, 83);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MainLabel);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -79,7 +79,7 @@ namespace Linkedin
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label MainLabel;
     }
 }
 
--- Linkedin/Form1.cs
+++ Linkedin/Form1.cs
@@ -12,6 +12,7 @@ using System.Windows.Forms;
 using Newtonsoft.Json;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 
 namespace Linkedin
@@ -21,24 +22,28 @@ namespace Linkedin
         private IWebDriver _driver { get; set; }
         private const string UserName = "akarelin19821982@gmail.com";
         private const string Password = "RPR4J3Jjpv";
+        private List<string> _accountLinks = new List<string>();
+        private int maxNumberOfAccount = 100;
 
         public Form1()
         {
             InitializeComponent();
-            Start(null,null);
+            Start(null, null);
         }
 
         private void Start(object sender, EventArgs e)
         {
             var chromeDriverService = ChromeDriverService.CreateDefaultService();
             chromeDriverService.HideCommandPromptWindow = true;
-            
+
             _driver = new ChromeDriver(chromeDriverService, new ChromeOptions());
             _driver.Url = "https://www.linkedin.com/login";
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
             IWait<IWebDriver> wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30.00));
 
-            wait.Until(driver1 => ((IJavaScriptExecutor)_driver).ExecuteScript("return document.readyState").Equals("complete"));
+            wait.Until(driver1 =>
+                ((IJavaScriptExecutor) _driver).ExecuteScript("return document.readyState").Equals("complete"));
 
             var emailInput = _driver.FindElement(By.Id("username"));
             emailInput.SendKeys(UserName);
@@ -48,12 +53,39 @@ namespace Linkedin
             var signInButton = _driver.FindElement(By.CssSelector("button[type='submit']"));
             signInButton.Click();
 
+            _driver.Url =
+                "https://www.linkedin.com/search/results/people/?geoUrn=%5B%22101282230%22%5D&keywords=dpo&network=%5B%22S%22%2C%22O%22%5D&origin=FACETED_SEARCH";
 
+            do
+            {
+                GetAllAccountsFromCurrentPage();
+                GoToAnotherPage();
+            } while (_accountLinks.Count < maxNumberOfAccount);
+
+            MainLabel.Text = "Sex";
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void GetAllAccountsFromCurrentPage()
         {
+            var accounts = _driver.FindElements(By.ClassName("entity-result__image")).ToList()
+                .Select(elem => elem.FindElement(By.ClassName("app-aware-link"))).ToList();
+            foreach (var account in accounts)
+            {
+                var link = account.GetAttribute("href");
+                _accountLinks.Add(link);
+                if (_accountLinks.Count == maxNumberOfAccount) return;
+            }
+        }
 
+        private void GoToAnotherPage()
+        {
+            var element = _driver.FindElement(By.Id("li-modal-container"));
+            Actions actions = new Actions(_driver);
+            actions.MoveToElement(element);
+            actions.Perform();
+
+            var nextPageButton = _driver.FindElement(By.CssSelector("button[aria-label=\"Далее\"]"));
+            nextPageButton.Click();
         }
     }
 }
